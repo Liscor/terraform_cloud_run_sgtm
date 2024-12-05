17bf2abcabe9f127e981c6171f1b59920d4b48fb
ChristianMohrStade <146724451+ChristianMohrStade@users.noreply.github.com>
2024-12-05 14:25:17 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,7 +1,10 @@
+#! /bin/bash
+
 #Initialize Terraform
 terraform init
 
 #Initialize GCloud
+gcloud auth application-default set-quota-project $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 gcloud config set project $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 gcloud services enable cloudresourcemanager.googleapis.com
 
