ffabcbbeb2cd42f79b08e97645cb4a8a6d68c338
ChristianMohrStade <146724451+ChristianMohrStade@users.noreply.github.com>
2024-12-06 10:26:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -13,3 +13,6 @@ https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt-get install -y terraform
+
+#GCloud LogIn
+gcloud auth application-default login
