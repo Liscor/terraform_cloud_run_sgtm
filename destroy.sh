2b5e500114464836cac33ae241d403e0822fec41
ChristianMohrStade <146724451+ChristianMohrStade@users.noreply.github.com>
2024-12-06 13:44:11 +0100

    added automatic state saving and destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,23 +1,16 @@
-#! /bin/bash
-
 #Initialize Terraform
 terraform init
 
 #Initialize GCloud
 gcloud auth application-default set-quota-project $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 gcloud config set project $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
-gcloud services enable cloudresourcemanager.googleapis.com
 
-#
+#Check for .tfstate Files
 if [ -d "$(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')" ]; then
     cp -R $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')/terraform.tfstate .
     cp -R $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')/terraform.tfstate.backup .
+    rm -r $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 fi
 
-#Apply Changes
-terraform apply
-
-#Move .tfstate Files do a dedicated Folder
-mkdir $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
-cp -R terraform.tfstate $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
-cp -R terraform.tfstate.backup $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
+#Destroy
+terraform destroy
