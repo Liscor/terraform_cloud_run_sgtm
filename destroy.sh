f6a482988775672ac873a99687bbd931f04c9c9d
ChristianMohrStade <146724451+ChristianMohrStade@users.noreply.github.com>
2024-12-06 15:14:51 +0100

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -9,8 +9,11 @@ gcloud config set project $(echo "var.project_id" | terraform console | tr -d '\
 if [ -d "$(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')" ]; then
     cp -R $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')/terraform.tfstate .
     cp -R $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')/terraform.tfstate.backup .
-    rm -r $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 fi
 
 #Destroy
 terraform destroy
+
+#if [ -d "$(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')" ]; then
+#    rm -r $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
+#fi
