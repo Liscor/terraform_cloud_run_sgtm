c755311bfe67e9d54c16d13bb7caff8719d5ade4
ChristianMohrStade <146724451+ChristianMohrStade@users.noreply.github.com>
2024-12-12 14:05:44 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -9,15 +9,18 @@ gcloud config set project $(echo "var.project_id" | terraform console | tr -d '\
 gcloud services enable cloudresourcemanager.googleapis.com
 
 #
-if [ -d "$(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')" ]; then
+if [ -d "$(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')" ]; 
+then
     cp -R $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')/terraform.tfstate .
     cp -R $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')/terraform.tfstate.backup .
+else
+    mkdir $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 fi
 
 #Apply Changes
 terraform apply
 
 #Move .tfstate Files do a dedicated Folder
-mkdir $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 cp -R terraform.tfstate $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
 cp -R terraform.tfstate.backup $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
+cp -R terraform.tfvars $(echo "var.project_id" | terraform console | tr -d '\n' | sed 's/^.//; s/.$//')
